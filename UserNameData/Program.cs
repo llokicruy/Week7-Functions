9fb6df16edad39daa4a25538f2f878fe54faf09a
Jüri Kollo <jkollo@tktk.ee>
2022-06-02 15:43:33 +0300

    add new projects

---
+++ UserNameData/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UserNameData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kusib kasutajal sisestada oma eesnimi
+            //programm kuvab kasutaja eesnime pikust
+            //programm kuvab kasutaja eesnime tagurpidi
+            //main meetodis ei tohi olla rohkem, kui 3 rida koodi
+
+            Console.WriteLine("sisesta oma eesnime:");
+            string userName = Console.ReadLine();
+
+            GetUserNameData(userName);
+        }
+
+        public static void GetUserNameData(string userInput)
+        {
+            Console.WriteLine($"sinu nimi on {userInput.Length} sumbolit pikk.");
+            Console.WriteLine($"sinu eesnime esimene taht on {userInput[0]}");
+            
+            for(int i =userInput.Length - 1; i >= 0; i --)
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+    }
+}
---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kusib kasutajal sisestada eesnime
+            //programm kusib kasutajal sisestada numbrit 1-3
+            //kui kasutaja valin 1, siis kuvatakse tema eesnimi tagurpidi
+            //kui kasutaja valib 2, siis kuvatakse kasutaja eesnime esimene taht
+            //kui kasutaja valib 3, siis kuvatakse kasutaja eesnime pikkust
+
+
+            Console.WriteLine("sisesta oma eesnime:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("vali number: (sisesta '1', '2' voi '3')");
+            char userOperator = Convert.ToChar(Console.ReadLine());
+
+
+            switch (userOperator)
+            {
+                case '1':
+                    Number1(userName);
+                    break;
+                case '2':
+                    Number2(userName);
+                    break;
+                case '3':
+                    Number3(userName);
+                    break;
+
+                default:
+                    Console.WriteLine("Kena päeva!");
+                    break;
+            }
+        }
+
+        public static void Number1(string userInput)
+        {
+
+
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+
+        public static void Number2(string userInput)
+        {
+            Console.WriteLine($"sinu eesnime esimene taht on {userInput[0]}");
+        }
+
+        public static void Number3(string userInput)
+        {
+            Console.WriteLine($"sinu nimi on {userInput.Length} sumbolit pikk.");
+        }
+
+    }
+}
